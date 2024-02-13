d0965dc042551e437052b847c715d29a541c9b6f
FrischSticks <119437787+FrischSticks@users.noreply.github.com>
2024-02-12 19:37:05 -0700

    Added Filter to Gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -9,11 +9,25 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 // RETRIEVES ALL IMAGES FROM IMAGES FOLDER
 const images = require.context('../images', true);
-const imageList = images.keys().map(image => images(image));
+const allImages = images.keys().map(image => images(image));
+
+console.log(allImages)
+
+// FILTERED IMAGES
+const deerImages = allImages.filter(image => image.includes('deer') || image.includes('Deer'));
+const fishImages = allImages.filter(image => image.includes('fish') || image.includes('Fish') ||
+  image.includes('bluegill') || image.includes('Bluegill') || 
+  image.includes('bass') || image.includes('Bass') || 
+  image.includes('crappie') || image.includes('Crappie'));
+const duckImages = allImages.filter(image => image.includes('duck') || image.includes('Duck'));
+const turkeyImages = allImages.filter(image => image.includes('turkey') || image.includes('Turkey'));
+const bearImages = allImages.filter(image => image.includes('bear') || image.includes('Bear'));
+const awardWinningImages = allImages.filter(image => image.includes('award') || image.includes('Award'));
+
 
 const Gallery = () => {
 
-  // console.log(imageList);
+  const [imageList, setImageList] = useState(allImages);
 
   const [slideNumber, setSlideNumber] = useState(0);
   const [openModal, setOpenModal] = useState(false);
@@ -46,7 +60,6 @@ const Gallery = () => {
     setOpenModal(false);
   }
 
-
   return (
     <div>
       <h1>Gallery</h1>
@@ -61,6 +74,17 @@ const Gallery = () => {
           </div>
         </div>
       }
+      <div class="dropdownFilter">
+          <select id="filterDropdown" onchange="updateFilter()">
+            <option value="All" onClick={ () => setImageList(allImages)}>All</option>
+            <option value="Award Winners" onClick={ () => setImageList( awardWinningImages )}>Award Winners</option>
+            <option value="Bear" onClick={ () => setImageList( bearImages )}>Bear</option>
+            <option value="Duck" onClick={ () => setImageList( duckImages )}>Duck</option>
+            <option value="Deer" onClick={ () => setImageList(deerImages)}>Deer</option>
+            <option value="Fish" onClick={ () => setImageList(fishImages)}>Fish</option>
+            <option value="Turkey" onClick={ () => setImageList(turkeyImages)}>Turkey</option>
+          </select>
+      </div>
       <div className="galleryWrap">
         {imageList &&
           imageList.map((slide, index) => {
